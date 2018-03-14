f3c66e2c228821d7cd20404bd386f17f0571c605
Jcalvarez889 <jcalvarez889@gmail.com>
2018-03-14 14:31:22 -0600

    boton

--- index.php
+++ index.php
@@ -9,18 +9,24 @@
 		<section class="main">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-4 offset-md-4">
+					<div class="col-md-4 offset-md-4" style="margin-top: 100px;">
 						<form class="login" id="login">
 							<div class="form-group text-center">
-								<img src="Imagenes/login.png" width="100px">
+								<img src="Imagenes/login.png" width="50%">
 							</div> 
 							<div class="form-group">               
-                            <div class="input-group">
-                                <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
-                                <input class="form-control input-lg" type="text" id="usuario" required placeholder="Usuario"/> 
+	                            <div class="input-group">
+	                                <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
+	                                <input class="form-control input-lg" type="text" id="usuario" required placeholder="Usuario"/>
+	                        	</div>
+                        	</div>  
+                        	<div class="form-group">               
+	                            <div class="input-group">
+	                                <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
+	                                <input class="form-control input-lg" type="password" id="contrasena" required placeholder="Contraseña"> 
+	                        	</div>
                         	</div>
-                        </div>  
-                        <div cl 
+                        	<button type="submit" class="btn btn-lg btn-block boton">Acceder</button>  
 						</form>
 					</div>
 				</div>
