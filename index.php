1a5e79c4e5fd6336399594e03278b1c79db27136
cecydb11 <jceciliadavilaarandas@gmail.com>
2018-03-14 10:46:19 -0600

    cambio del index

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+		<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css" type="text/css">
+		<link rel="stylesheet" href="CSS/login.css">
+	</head>
+	<body>
+		<section class="main">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 offset-md-4">
+						<form class="login" id="login">
+							<div class="form-group text-center">
+								<img src="Imagenes/login.png" width="100px">
+							</div> 
+							<div class="form-group">               
+                            <div class="input-group">
+                                <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
+                                <input class="form-control input-lg" type="text" id="usuario" required placeholder="Usuario"/> 
+                        	</div>
+                        </div>  
+                        <div cl 
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+		<script type="text/javascript" src="JS/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="JS/login.js"></script>
+	</body>
+</html>
